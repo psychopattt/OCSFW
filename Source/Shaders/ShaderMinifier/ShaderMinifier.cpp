02dc759cc159387e8b1d228ea12a056f6a9c4461
Psycho <56837234+psychopattt@users.noreply.github.com>
2025-05-04 13:42:00 -0400

    Minifier: Rename variables

--- Source/Shaders/ShaderMinifier/ShaderMinifier.cpp
+++ Source/Shaders/ShaderMinifier/ShaderMinifier.cpp
@@ -17,6 +17,7 @@ namespace ShaderMinifier
 		RenameStructs(shader);
 		RenameDefines(shader);
 		RenameFunctions(shader);
+		RenameVariables(shader);
 		EscapeNewlines(shader);
 	}
 
@@ -38,8 +39,10 @@ namespace ShaderMinifier
 		const regex structRegex(R"(^.*?\bstruct\s+([a-zA-Z_]\w*)\s*\{)");
 		const regex defineRegex(R"(^#define\s+([^\s]+))");
 		const regex functionRegex(R"(^.*?\b([a-z_]\w*)\s+([a-z_]\w*)\s*\()", regex::icase);
+		const regex variableRegex(R"(^.*?\b\s*(?:(?:in|out|inout)\s+)?([a-zA-Z_]\w*)\s+([a-zA-Z_]\w*)\s*[;,=)])");
 		const regex returnRegex(R"(^return$)");
 		const regex mainRegex(R"(^main$)");
+		const regex groupRegex(R"([{}()])");
 		const regex newlineRegex(R"(\n)");
 
 		void ResetIdentifiers()
@@ -57,7 +60,7 @@ namespace ShaderMinifier
 			const std::vector<string> directives = ExtractDirectives(shader);
 			shader = regex_replace(shader, tokenSpaceRegex, "$1");
 			InsertDirectives(shader, directives);
-
+			
 			shader = regex_replace(shader, leadingTrailingSpaceRegex, "$1$2$3");
 			shader = regex_replace(shader, extraSpaceRegex, " ");
 			shader = regex_replace(shader, extraNewlineRegex, "\n");
@@ -113,14 +116,15 @@ namespace ShaderMinifier
 
 			while (regex_search(shader.cbegin() + offset, shader.cend(), match, structRegex))
 			{
+				size_t matchEnd = match.position() + match.length();
+
 				if (regex_search(match[0].first, match[0].second, directiveRegex))
 				{
-					offset += match.position() + match.length();
+					offset += matchEnd;
 					continue;
 				}
 
 				string matchedStruct = match.str(1);
-				size_t matchEnd = match.position() + match.length();
 				string replacement = GenerateUniqueIdentifier(shader);
 
 				const regex matchedStructRegex(R"(([^\.\w]))" + matchedStruct + "\\b");
@@ -153,21 +157,100 @@ namespace ShaderMinifier
 
 			while (regex_search(shader.cbegin() + offset, shader.cend(), match, functionRegex))
 			{
+				size_t matchEnd = match.position() + match.length();
+
 				if (regex_search(match[0].first, match[0].second, directiveRegex) ||
 					regex_search(match[1].first, match[1].second, returnRegex) ||
 					regex_search(match[2].first, match[2].second, mainRegex))
 				{
-					offset += match.position() + match.length();
+					offset += matchEnd;
 					continue;
 				}
 
 				string matchedFunction = match.str(2);
-				size_t matchEnd = match.position() + match.length();
 				string replacement = GenerateUniqueIdentifier(shader);
 
 				const regex matchedFunctionRegex(R"(([^\.\w]))" + matchedFunction + "\\b");
 				shader = regex_replace(shader, matchedFunctionRegex, "$1" + replacement);
 				offset += matchEnd - matchedFunction.length() + replacement.length();
+			}
+		}
+
+		void RenameVariables(string& shader)
+		{
+			std::smatch match;
+			size_t offset = 0;
+
+			while (regex_search(shader.cbegin() + offset, shader.cend(), match, functionRegex))
+			{
+				offset += match.position() + match.length();
+
+				if (!regex_search(match[0].first, match[0].second, directiveRegex) &&
+					!regex_search(match[1].first, match[1].second, returnRegex))
+				{
+					size_t functionStart = offset;
+					AdvanceToNextFunctionEnd(shader, offset);
+					RenameFunctionVariables(shader, functionStart, offset);
+				}
+			}
+		}
+
+		void AdvanceToNextFunctionEnd(const string& shader, size_t& offset)
+		{
+			std::smatch match;
+			int bracketDepth = 0;
+			int parenthesisDepth = 1;
+			bool bodyFound = false;
+			bool endFound = false;
+
+			while (!endFound && regex_search(shader.cbegin() + offset, shader.cend(), match, groupRegex))
+			{
+				string symbol = match.str();
+				parenthesisDepth += (symbol == "(") - (symbol == ")");
+				bracketDepth += (symbol == "{") - (symbol == "}");
+
+				bodyFound = bodyFound || (parenthesisDepth == 0 && bracketDepth == 1);
+				endFound = bodyFound && parenthesisDepth == 0 && bracketDepth == 0;
+
+				offset += match.position() + match.length();
+			}
+		}
+
+		void RenameFunctionVariables(string& shader, size_t functionStart, size_t& functionEnd)
+		{
+			std::smatch match;
+			size_t offset = functionStart;
+
+			while (regex_search(shader.cbegin() + offset, shader.cbegin() + functionEnd, match, variableRegex))
+			{
+				size_t matchEnd = match.position() + match.length();
+
+				if (regex_search(match[0].first, match[0].second, directiveRegex) ||
+					regex_search(match[1].first, match[1].second, returnRegex) ||
+					match.length(2) == 1)
+				{
+					offset += matchEnd;
+					continue;
+				}
+
+				string matchedVariable = match.str(2);
+				string replacement = GenerateUniqueIdentifier(shader);
+				ReplaceFunctionVariable(shader, offset, functionEnd, matchedVariable, replacement);
+				offset += matchEnd - matchedVariable.length() + replacement.length();
+			}
+		}
+
+		void ReplaceFunctionVariable(string& shader, size_t start, size_t& end,
+			const string& variable, const string& replacement)
+		{
+			std::smatch match;
+			const regex matchRegex(R"(([^\.\w]))" + variable + "\\b");
+
+			while (regex_search(shader.cbegin() + start, shader.cbegin() + end, match, matchRegex))
+			{
+				end -= variable.length() - replacement.length();
+				start += match.position() + match.length() - variable.length() + replacement.length();
+				shader.replace(match[0].first, match[0].second, match.str(1) + replacement);
 			}
 		}
 
--- Source/Shaders/ShaderMinifier/ShaderMinifier.h
+++ Source/Shaders/ShaderMinifier/ShaderMinifier.h
@@ -22,6 +22,11 @@ namespace ShaderMinifier
 		void RenameStructs(string& shader);
 		void RenameDefines(string& shader);
 		void RenameFunctions(string& shader);
+		void RenameVariables(string& shader);
+		void AdvanceToNextFunctionEnd(const string& shader, size_t& offset);
+		void RenameFunctionVariables(string& shader, size_t functionStart, size_t& functionEnd);
+		void ReplaceFunctionVariable(string& shader, size_t start, size_t& end,
+			const string& variable, const string& replacement);
 		const string GenerateUniqueIdentifier(const string& shader);
 		const string GenerateIdentifier(size_t id);
 		void EscapeNewlines(string& shader);
